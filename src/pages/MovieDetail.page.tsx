479c8dffe67130f79ac0c4f8bc243211a1ad173a
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2024-02-22 00:51:32 +0100

    Git hub action added with git hub pages

--- src/pages/MovieDetail.page.tsx
+++ src/pages/MovieDetail.page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MovieDetailPage: React.FC = () => {
-    return <div>Movie Detailed page</div>;
+    return <div>Movie Detailed page test</div>;
 };
 
 export default MovieDetailPage;
